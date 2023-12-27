bc4cbaa880fbfae0bb75f5228fc8ae9aa684ec1a
Adriano Nobre <adrianonobrevg02@gmail.com>
2023-12-27 10:51:16 -0400

    first commmit

---
+++ backend/config/generateToken.js
@@ -0,0 +1,9 @@
+const jwt = require("jsonwebtoken");
+const chave = 'piyush'
+const generateToken = (id) => {
+  return jwt.sign({ id }, chave, {
+    expiresIn: "30d",
+  });
+};
+
+module.exports = generateToken;
---
+++ frontend/src/Pages/Homepage.js
@@ -0,0 +1,64 @@
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useHistory } from "react-router";
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
+import { Image } from "@chakra-ui/react";
+import CryptoChatLogo from '../components/miscellaneous/chatcoin.svg';
+
+function Homepage() {
+  const history = useHistory();
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("userInfo"));
+
+    if (user) history.push("/chats");
+  }, [history]);
+
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Image src={CryptoChatLogo} alt="CryptoChat Logo" boxSize="50px" objectFit="contain" mr='20px' />
+        <Text fontSize="4xl" fontFamily="Work sans">
+          CryptoChat
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs isFitted variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab>Login</Tab>
+            <Tab>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+}
+
+export default Homepage;
